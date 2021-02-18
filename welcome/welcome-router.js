b114e9f1b9a07d927ab4333c3f88867f6669f08d
Sean Kramer <seandominickramer@gmail.com>
2021-02-17 20:57:58 -0500

    Change welcome message again

--- welcome/welcome-router.js
+++ welcome/welcome-router.js
@@ -6,6 +6,7 @@ router.get("/", (req, res) => {
 	res.status(200).json({
 		message: `Welcome ${process.env.COHORT}`,
 		fact: process.env.FUN_FACT || "mum",
+		heroku_port: process.env.PORT,
 	})
 })
 
